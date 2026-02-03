03886f23b2c8ef0f2804aee1c8a47146c44cad69
Eduardo Jimenez <edumarji.tmr@gmail.com>
2026-02-02 20:02:21 -0500

    Optimize sleep code

--- Sleep.js
+++ Sleep.js
@@ -1,14 +1,5 @@
-function sleep(milli) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(milli);
-    }, milli);
-  });
-}
-
-// Ejemplo de uso
-sleep(1000).then((valor) => {
-  console.log("Pasaron", valor, "ms");
-});
-
+(async () => {
+  const ms = await sleep(1000);
+  console.log("Pasaron", ms, "ms");
+})();
 
